cae9702feec74650a2e3e6a1e30d0424c45d855a
zakafir <zak.afir@gmail.com>
2017-07-27 06:42:00 +0200

    MediaPlayer Demo

--- app/src/main/java/com/st00/afir/musicplayerdemo/MainActivity.java
+++ app/src/main/java/com/st00/afir/musicplayerdemo/MainActivity.java
@@ -1,13 +1,80 @@
 package com.st00.afir.musicplayerdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button playMusic, pauseMusic,increaseVolume, decreaseVolume, goToMiddle;
+    //creating a mediaPlayer
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+    int duration = 0;
+    int volume = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playMusic = (Button) findViewById(R.id.playButton);
+        pauseMusic = (Button) findViewById(R.id.pauseButton);
+
+        increaseVolume = (Button) findViewById(R.id.plusButton);
+        decreaseVolume = (Button) findViewById(R.id.minusButton);
+
+        goToMiddle = (Button) findViewById(R.id.goToButton);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.chauffeur_capable);
+        duration = mediaPlayer.getDuration();
+
+        playMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        pauseMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        volume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        Log.d("Main","volume est :"+volume);
+
+        increaseVolume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                volume++;
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+                Log.d("Main","volume incrémenté :"+volume);
+            }
+        });
+
+        decreaseVolume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                volume--;
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+                Log.d("Main","volume décrementé :"+volume);
+            }
+        });
+
+        goToMiddle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.seekTo(duration/2);
+            }
+        });
     }
 }
